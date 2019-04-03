ae9c293da0147193c07040a2d26d240c3571f557
g0lo8a <38590113+g0lo8a@users.noreply.github.com>
2019-04-03 12:32:19 +0300

    Внёс правки

--- html-element-collection/audioplayer/script.js
+++ html-element-collection/audioplayer/script.js
@@ -1,50 +1,47 @@
 'use strict';
 
 const player = document.getElementsByClassName('audio-player')[0];
-
 const Title = document.getElementsByClassName('title')[0];
-
 const src = [
-    "LA Chill Tour",
-    "LA Fusion Jam",
-    "This is it band"
+    'LA Chill Tour',
+    'LA Fusion Jam',
+    'This is it band'
 ];
-
 let i = 0;
 player.src = 'mp3/' + src[i] + '.mp3';
-
 const mediaPlayer = document.getElementsByClassName('mediaplayer')[0];
 
-document.getElementsByClassName('fa-play')[0].onclick = () => {
-    player.play();
+function switchSongs() {
+    player.src = 'mp3/' + src[i] + '.mp3';
+    Title.setAttribute('title', src[i]);
+    if (document.getElementsByClassName('play')[0]) {
+        player.play();
+    } else {
+        player.pause();
+    }
+}
+
+document.getElementsByClassName('back')[0].onclick = () => {
+    i === 0 ? i = src.length - 1 : i--;
+    switchSongs();
+};
+
+document.getElementsByClassName('playstate')[0].onclick = () => {
+    if (document.getElementsByClassName('play')[0]) {
+        player.pause();
+    } else {
+        player.play();
+    }
     mediaPlayer.classList.toggle('play');
 };
 
-document.getElementsByClassName('fa-pause')[0].onclick = () => {
-    player.pause();
-    mediaPlayer.classList.toggle('play');
-};
-
-document.getElementsByClassName('fa-stop')[0].onclick = () => {
+document.getElementsByClassName('stop')[0].onclick = () => {
     player.pause();
     player.currentTime = 0;
     mediaPlayer.classList.remove('play');
 };
 
-document.getElementsByClassName('back')[0].onclick = () => {
-    mediaPlayer.classList.remove('play');
-    i === 0 ? i = src.length - 1 : i--;
-    player.src = 'mp3/' + src[i] + '.mp3';
-    player.play();
-    mediaPlayer.classList.toggle('play');
-    Title.setAttribute('title', src[i]);
-};
-
 document.getElementsByClassName('next')[0].onclick = () => {
-    mediaPlayer.classList.remove('play');
     i === src.length - 1 ? i = 0 : i++;
-    player.src = 'mp3/' + src[i] + '.mp3';
-    player.play();
-    mediaPlayer.classList.toggle('play');
-    Title.setAttribute('title', src[i]);
+    switchSongs();
 };
