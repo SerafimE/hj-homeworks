c6d2ad3e18d06dce77d619f8ab497a7d97960179
g0lo8a <38590113+g0lo8a@users.noreply.github.com>
2019-04-09 20:05:51 +0300

    На проверку

--- xhr/books-catalog/js/script.js
+++ xhr/books-catalog/js/script.js
@@ -1,1 +1,33 @@
 'use strict';
+
+const ul = document.querySelector('#content');
+const li = document.querySelector('#content li');
+li.parentNode.removeChild(li);
+
+const xhr = new XMLHttpRequest();
+xhr.addEventListener('load', onLoad);
+xhr.open('GET', 'https://neto-api.herokuapp.com/book/');
+xhr.send();
+
+const result = [];
+
+function onLoad() {
+
+    let data = JSON.parse(xhr.responseText);
+    for (let k in data) {
+        let v = data[k];
+        result.push(v);
+    }
+
+    for (let i = 0; i < result.length; i++) {
+
+        const li = document.createElement('li');
+
+        li.dataset.title = result[i].title;
+        li.dataset.author = result[i].author.name;
+        li.dataset.info = result[i].info;
+        li.dataset.price = result[i].price;
+        ul.append(li);
+        li.appendChild((document.createElement('img'))).src = result[i].cover.small;
+    }
+}
