b693b2df0fd6421a9bd44dc270a33cf52cc2195a
g0lo8a <38590113+g0lo8a@users.noreply.github.com>
2019-04-16 18:30:50 +0300

    На проверку

--- event-bubbling-capturing/shop/js/cart.js
+++ event-bubbling-capturing/shop/js/cart.js
@@ -1,1 +1,12 @@
 'use strict';
+
+const listItem = document.querySelector('.items-list');
+listItem.addEventListener('click', event => {
+    event.preventDefault();
+    event.stopPropagation();
+    const item = {
+        title: event.target.dataset.title,
+        price: event.target.dataset.price
+    };
+    addToCart(item);
+});
