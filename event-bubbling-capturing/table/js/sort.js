cbdd7171b12e51c4ae5a1edd74c66d9869521ccf
g0lo8a <38590113+g0lo8a@users.noreply.github.com>
2019-04-16 18:29:14 +0300

    На проверку

--- event-bubbling-capturing/table/js/sort.js
+++ event-bubbling-capturing/table/js/sort.js
@@ -1,5 +1,7 @@
 'use strict';
 
 function handleTableClick(event) {
-  
+    const field = event.currentTarget.dataset.sortBy = event.target.dataset.propName;
+    const direction = (event.target.dataset.dir = (!event.target.dataset.dir) ? 1 : event.target.dataset.dir * -1);
+    sortTable(field, direction);
 }
