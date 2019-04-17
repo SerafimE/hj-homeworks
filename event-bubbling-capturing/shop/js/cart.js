aafa93b5237080de8c8f87d5c7e5ca5a9e1f6f3e
g0lo8a <38590113+g0lo8a@users.noreply.github.com>
2019-04-17 12:04:57 +0300

    Поправил

--- event-bubbling-capturing/shop/js/cart.js
+++ event-bubbling-capturing/shop/js/cart.js
@@ -4,11 +4,12 @@ const listItem = document.querySelector('.items-list');
 
 listItem.addEventListener('click', event => {
     event.preventDefault();
-    const item = {
-        title: event.target.dataset.title,
-        price: event.target.dataset.price
-    };
+
     if (event.target.className === 'add-to-cart') {
+        const item = {
+            title: event.target.dataset.title,
+            price: event.target.dataset.price
+        };
         addToCart(item);
     }
 });
