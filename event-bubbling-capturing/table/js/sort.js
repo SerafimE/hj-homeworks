efa08367488be43f3b50d6c8a5bd50e828328a9a
g0lo8a <38590113+g0lo8a@users.noreply.github.com>
2019-04-17 11:37:15 +0300

    Поправил

--- event-bubbling-capturing/table/js/sort.js
+++ event-bubbling-capturing/table/js/sort.js
@@ -1,7 +1,12 @@
 'use strict';
 
+const handles = document.querySelectorAll('tr > th');
+for (let handle of handles) {
+    handle.addEventListener('click', handleTableClick)
+}
+
 function handleTableClick(event) {
-    const field = event.currentTarget.dataset.sortBy = event.target.dataset.propName;
-    const direction = (event.target.dataset.dir = (!event.target.dataset.dir) ? 1 : event.target.dataset.dir * -1);
-    sortTable(field, direction);
+    event.currentTarget.dataset.sortBy = event.target.dataset.propName;
+    event.currentTarget.dataset.dir = event.target.dataset.dir === '1' ? '-1' : '1';
+    sortTable(event.currentTarget.dataset.sortBy, event.target.dataset.dir);
 }
