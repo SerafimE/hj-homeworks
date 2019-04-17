a1f06b5b16e016087a1b3557fb92d4b4e8c89489
g0lo8a <38590113+g0lo8a@users.noreply.github.com>
2019-04-17 09:21:49 +0300

    На проверку

--- local-storage/counter/js/counter.js
+++ local-storage/counter/js/counter.js
@@ -1,1 +1,24 @@
 'use strict';
+
+const increment = document.querySelector('#increment'),
+    decrement = document.querySelector('#decrement'),
+    reset = document.querySelector('#reset'),
+    counter = document.querySelector('#counter');
+let localStorageCounter = localStorage.getItem('counter');
+counter.textContent = localStorageCounter;
+
+increment.addEventListener('click', event => count());
+decrement.addEventListener('click', event => count());
+reset.addEventListener('click', event => count());
+
+function count() {
+    if (event.target === increment) {
+        ++localStorageCounter;
+    } else if (event.target === decrement) {
+        --localStorageCounter;
+    } else if (event.target === reset) {
+        localStorageCounter = 0;
+    }
+    counter.textContent = localStorageCounter;
+    localStorage.setItem('counter', localStorageCounter);
+}
