335ba629e20c9c9099e046340d2f75d8e1aeea06
g0lo8a <38590113+g0lo8a@users.noreply.github.com>
2019-04-19 16:41:39 +0300

    Внёс правки

--- html-forms/feedback/script.js
+++ html-forms/feedback/script.js
@@ -1,28 +1,29 @@
 'use strict';
 
-const inputCollection = document.querySelectorAll('input');
+const inputCollection = document.querySelectorAll('input, textarea');
 const form = document.querySelector('.contentform');
 const message = document.querySelector('#output');
 const buttonSend = document.querySelectorAll('.button-contact')[0];
 const buttonCheck = document.querySelectorAll('.button-contact')[1];
-const textArea = document.querySelector('textarea');
 let count = 0;
 
 for (let t of inputCollection) {
     if (t.name === 'zip') {
-        t.type = 'number';
+        t.addEventListener('input', event => {
+            t.value = t.value.replace(/\D/, '');
+        });
     }
 
-    t.addEventListener('change', function () {
+    t.addEventListener('input', event => {
+        if (t.value === '') {
+            buttonSend.disabled = true;
+            --count;
+        }
+    });
+    t.addEventListener('change', event => {
         if (t.value !== '') {
             ++count;
-            document.getElementById(t.name).value = t.value;
         }
-
-        textArea.addEventListener('change', function () {
-            document.getElementById(textArea.name).value = textArea.value;
-        });
-
         if (count === inputCollection.length) {
             buttonSend.disabled = false;
             buttonSend.type = 'button';
@@ -30,12 +31,12 @@ for (let t of inputCollection) {
     });
 }
 
-buttonSend.addEventListener('click', function () {
+buttonSend.addEventListener('click', event => {
     form.classList.add('hidden');
     message.classList.remove('hidden');
 });
 
-buttonCheck.addEventListener('click', function () {
+buttonCheck.addEventListener('click', event => {
     form.classList.remove('hidden');
     message.classList.add('hidden');
 });
