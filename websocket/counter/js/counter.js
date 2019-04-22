cfbce173de92497737f19e16edbeec40046e5464
g0lo8a <38590113+g0lo8a@users.noreply.github.com>
2019-04-22 16:48:19 +0300

    На проверку

--- websocket/counter/js/counter.js
+++ websocket/counter/js/counter.js
@@ -1,1 +1,13 @@
 'use strict';
+
+const connect = new WebSocket('wss://neto-api.herokuapp.com/counter');
+
+const counter = document.querySelector('.counter');
+const output = document.querySelector('output.errors');
+
+connect.addEventListener('message', event => {
+    counter.textContent = JSON.parse(event.data).connections;
+    output.value = JSON.parse(event.data).errors;
+});
+
+window.addEventListener('beforeunload', event => connect.close(1000));
