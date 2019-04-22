62771812d8ba3ca220b8c7738b6bc0e635cfaaba
g0lo8a <38590113+g0lo8a@users.noreply.github.com>
2019-04-22 15:54:04 +0300

    На проверку

--- websocket/bubbles/js/click.js
+++ websocket/bubbles/js/click.js
@@ -1,1 +1,23 @@
 'use strict';
+
+let coords = {};
+
+const connection = new WebSocket('wss://neto-api.herokuapp.com/mouse');
+
+const click = window.addEventListener('click', event => {
+
+    coords = {
+        x: event.pageX,
+        y: event.pageY
+    };
+    connection.send(JSON.stringify(coords));
+});
+connection.addEventListener('open', () => {
+    console.log('Вебсокет-соединение открыто');
+});
+
+connection.addEventListener('message', event => {
+    console.log(`Получено сообщение: ${event.data}`);
+});
+
+showBubbles(connection);
