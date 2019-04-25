8ac09196f37cb504ae0cec0b259a354c208e1c75
g0lo8a <38590113+g0lo8a@users.noreply.github.com>
2019-04-25 11:03:12 +0300

    На проверку

--- cors/twitter/js/twitter.js
+++ cors/twitter/js/twitter.js
@@ -1,1 +1,25 @@
 'use strict';
+
+function loadData(url) {
+    const functionName = 'functionName' + (Math.random() * 1000).toFixed();
+    return new Promise((done, fail) => {
+        window[functionName] = done;
+        const script = document.createElement('script');
+        script.src = `${url}?jsonp=${functionName}`;
+        document.body.appendChild(script);
+    });
+}
+
+function showProfile(profile) {
+    document.querySelector('[data-wallpaper]').src = profile.wallpaper;
+    document.querySelector('[data-pic]').src = profile.pic;
+    document.querySelector('[data-username]').textContent = profile.username;
+    document.querySelector('[data-description]').textContent = profile.description;
+    document.querySelector('[data-tweets]').textContent = profile.tweets;
+    document.querySelector('[data-followers]').textContent = profile.followers;
+    document.querySelector('[data-following]').textContent = profile.following;
+}
+
+loadData('https://neto-api.herokuapp.com/twitter/jsonp')
+    .then(showProfile)
+    .catch(error => console.log(error));
