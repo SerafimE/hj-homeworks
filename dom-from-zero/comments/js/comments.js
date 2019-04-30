2bfa9741a9e971d2d0ee7813f61f55589e910f5b
g0lo8a <38590113+g0lo8a@users.noreply.github.com>
2019-04-30 17:14:51 +0300

    На проверку

--- dom-from-zero/comments/js/comments.js
+++ dom-from-zero/comments/js/comments.js
@@ -1,31 +1,57 @@
 'use strict';
 
-function showComments(list) {
-  const commentsContainer = document.querySelector('.comments');
-  const comments = list.map(createComment).join('');
-  commentsContainer.innerHTML += comments;
+function createComment(comment) {
+    const element = document.createElement('div');
+    element.className = 'comment-wrap';
+    const photo = document.createElement('div');
+    photo.className = 'photo';
+    photo.title = comment.author.name;
+    const avatar = document.createElement('div');
+    avatar.className = 'avatar';
+    avatar.style.backgroundImage = `url(${comment.author.pic})`;
+    const commentBlock = document.createElement('div');
+    commentBlock.className = 'comment-block';
+    const commentText = document.createElement('p');
+    commentText.className = 'comment-text';
+    commentText.innerText = comment.text.split('\\n').join('<br>');
+    const bottomComment = document.createElement('div');
+    bottomComment.className = 'bottom-comment';
+    const commentDate = document.createElement('div');
+    commentDate.className = 'comment-date';
+    commentDate.textContent = new Date(comment.date).toLocaleString('ru-Ru');
+    const commentAction = document.createElement('ul');
+    commentAction.className = 'comment-actions';
+    const complain = document.createElement('li');
+    complain.className = 'complain';
+    complain.innerText = 'Пожаловаться';
+    const reply = document.createElement('li');
+    reply.className = 'reply';
+    reply.innerText = 'Ответить';
+
+    element.appendChild(photo);
+    photo.appendChild(avatar);
+    element.appendChild(commentBlock);
+    commentBlock.appendChild(commentText);
+    commentBlock.appendChild(bottomComment);
+    bottomComment.appendChild(commentDate);
+    bottomComment.appendChild(commentAction);
+    commentAction.appendChild(complain);
+    commentAction.appendChild(reply);
+
+    return element;
 }
 
-function createComment(comment) {
-  return `<div class="comment-wrap">
-    <div class="photo" title="${comment.author.name}">
-      <div class="avatar" style="background-image: url('${comment.author.pic}')"></div>
-    </div>
-    <div class="comment-block">
-      <p class="comment-text">
-        ${comment.text.split('\n').join('<br>')}
-      </p>
-      <div class="bottom-comment">
-        <div class="comment-date">${new Date(comment.date).toLocaleString('ru-Ru')}</div>
-        <ul class="comment-actions">
-          <li class="complain">Пожаловаться</li>
-          <li class="reply">Ответить</li>
-        </ul>
-      </div>
-    </div>
-  </div>`
+function showComments(list) {
+    const fragment = document.createDocumentFragment();
+    list.forEach(li => {
+        fragment.appendChild(
+            createComment(li),
+        )
+    });
+    document.querySelector('.comments').appendChild(fragment);
 }
+
 
 fetch('https://neto-api.herokuapp.com/comments')
-  .then(res => res.json())
-  .then(showComments);
+    .then(res => res.json())
+    .then(showComments);
