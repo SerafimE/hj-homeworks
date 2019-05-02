59a8635f7a9d2a8ee1d97ffc6ec090d8a7b6b6c8
g0lo8a <38590113+g0lo8a@users.noreply.github.com>
2019-05-02 18:32:34 +0300

    На проверку

--- dom-from-zero/store/js/dom.js
+++ dom-from-zero/store/js/dom.js
@@ -1,1 +1,15 @@
 'use strict';
+
+function createElement(node) {
+    if (typeof node === 'string') {
+        return document.createTextNode(node);
+    }
+    const element = document.createElement(node.name);
+    if (node.props && typeof node.props === 'object') {
+        Object.keys(node.props).forEach(i => element.setAttribute(i, node.props[i]));
+    }
+    if (node.childs instanceof Array) {
+        node.childs.forEach(child => element.appendChild(createElement(child)));
+    }
+    return element;
+}
